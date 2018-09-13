d827f072221eb389eb075e087b1d5b95f3db945e
Ulrich Leodolter <ulrich.leodolter@obvsg.at>
2018-09-13 13:45:10 +0200

    fix UID docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,15 +5,16 @@ DEFAULT_USER=node
 DEFAULT_UID=$(id -u $DEFAULT_USER)
 DEFAULT_GID=$(id -g $DEFAULT_USER)
 
-DOCKER_UID=${UID:-$DEFAULT_UID}
-DOCKER_GID=${GID:-$DEFAULT_GID}
-
-if [[ "$DOCKER_UID" != "$DEFAULT_UID" ]]; then
-    usermod -o -u $DOCKER_UID $DEFAULT_USER
-fi
+DOCKER_UID=${DOCKER_UID:-$DEFAULT_UID}
+DOCKER_GID=${DOCKER_GID:-$DEFAULT_GID}
 
 if [[ "$DOCKER_GID" != "$DEFAULT_GID" ]]; then
-    groupmod -o -g $DOCKER_GID $DEFAULT_USER
+    addgroup -g $DOCKER_GID docker
+fi
+
+if [[ "$DOCKER_UID" != "$DEFAULT_UID" ]]; then
+    DOCKER_GROUP=$(getent group $DOCKER_GID | cut -d: -f1)
+    adduser -G $DOCKER_GROUP -s /bin/bash -u $DOCKER_UID -g "Docker User" -D docker
 fi
 
 exec su-exec $DOCKER_UID:$DOCKER_GID "$@"
