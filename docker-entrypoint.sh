9fbcae31afd85aae80b59def2accdefe172c305e
Ulrich Leodolter <ulrich.leodolter@obvsg.at>
2018-10-08 12:57:23 +0200

    remove user before creating

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,29 +5,31 @@ DEFAULT_USER=node
 DEFAULT_UID=$(id -u $DEFAULT_USER)
 DEFAULT_GID=$(id -g $DEFAULT_USER)
 
-DOCKER_UID=${DOCKER_UID:-$DEFAULT_UID}
-DOCKER_GID=${DOCKER_GID:-$DEFAULT_GID}
+DEVENV_USER=devenv
+DEVENV_UID=${DEVENV_UID:-$DEFAULT_UID}
+DEVENV_GID=${DEVENV_GID:-$DEFAULT_GID}
 
-if [[ "$DOCKER_GID" != "$DEFAULT_GID" ]]; then
-    if getent group docker >/dev/null; then
-        delgroup docker
-    fi
-    addgroup -g $DOCKER_GID docker
+if getent passwd $DEVENV_USER >/dev/null; then
+    deluser --remove-home $DEVENV_USER
+fi
+if getent group $DEVENV_USER >/dev/null; then
+    delgroup $DEVENV_USER
 fi
 
-DOCKER_GROUP=$(getent group $DOCKER_GID | cut -d: -f1)
-
-if [[ "$DOCKER_UID" != "$DEFAULT_UID" ]]; then
-    if getent passwd docker >/dev/null; then
-        deluser --remove-home docker
-    fi
-    adduser -G $DOCKER_GROUP -s /bin/bash -u $DOCKER_UID -g "Docker User" -D docker
+if [[ "$DEVENV_GID" != "$DEFAULT_GID" ]]; then
+    addgroup -g $DEVENV_GID $DEVENV_USER
 fi
 
-DOCKER_USER=$(getent passwd $DOCKER_UID | cut -d: -f1)
+DEVENV_GROUP=$(getent group $DEVENV_GID | cut -d: -f1)
+
+if [[ "$DEVENV_UID" != "$DEFAULT_UID" ]]; then
+    adduser -G $DEVENV_GROUP -s /bin/bash -u $DEVENV_UID -g "Devenv User" -D $DEVENV_USER
+fi
+
+DEVENV_USER=$(getent passwd $DEVENV_UID | cut -d: -f1)
 
 # allow creation of files and directories
-chgrp $DOCKER_GROUP . node_modules
+chgrp $DEVENV_GROUP . node_modules
 chmod 775 . node_modules
 
-exec su-exec $DOCKER_UID:$DOCKER_GID "$@"
+exec su-exec $DEVENV_UID:$DEVENV_GID "$@"
