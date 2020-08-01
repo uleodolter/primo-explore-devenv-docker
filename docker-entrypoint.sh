1a4f482333de47eb560cb82f0abd0ab1276c91fe
Ulrich Leodolter <ulrich.leodolter@obvsg.at>
2020-08-01 20:26:07 +0200

    fix user in entry script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,22 +10,20 @@ DEVENV_UID=${DEVENV_UID:-$DEFAULT_UID}
 DEVENV_GID=${DEVENV_GID:-$DEFAULT_GID}
 
 if getent passwd $DEVENV_USER >/dev/null; then
-    deluser --remove-home $DEVENV_USER
+    userdel --remove $DEVENV_USER
 fi
 if getent group $DEVENV_USER >/dev/null; then
-    delgroup $DEVENV_USER
+    groupdel $DEVENV_USER
 fi
 
 if [[ "$DEVENV_GID" != "$DEFAULT_GID" ]]; then
-    addgroup -g $DEVENV_GID $DEVENV_USER
+    groupadd -g $DEVENV_GID $DEVENV_USER
 fi
 
 DEVENV_GROUP=$(getent group $DEVENV_GID | cut -d: -f1)
 
 if [[ "$DEVENV_UID" != "$DEFAULT_UID" ]]; then
-# Alpine
-#   adduser -G $DEVENV_GROUP -s /bin/bash -u $DEVENV_UID -g "Devenv User" -D $DEVENV_USER
-    useradd -G $DEVENV_GROUP -s /bin/bash -u $DEVENV_UID -c "Devenv User" -D $DEVENV_USER
+    useradd -D -G $DEVENV_GROUP -s /bin/bash -u $DEVENV_UID -c "Devenv User" $DEVENV_USER
 fi
 
 DEVENV_USER=$(getent passwd $DEVENV_UID | cut -d: -f1)
